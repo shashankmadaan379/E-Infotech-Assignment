31577df9c49c9a453eec13c9234173310f6a9c29
shashankmadaan379 <codebreaker379@gmail.com>
2023-05-07 18:05:09 +0530

    added css to login and register

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,43 +1,50 @@
 import React from "react";
-import { Link } from "react-router-dom";
-
+import singupimage from "../assets/image1.jpg";
 const Login = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
   return (
     <div className="container">
       <div className="modal">
         <div className="modal-container">
-          <h1 className="modal-title">Welcome!</h1>
-          <form>
-            <div className="input-block">
-              <label htmlFor="email" className="input-label">
-                Email
-              </label>
-              <input
-                type="email"
-                autoComplete="off"
-                name="email"
-                placeholder="Email"
-                id="email"
-              />
-            </div>
-            <div className="input-block">
-              <label htmlFor="password" className="input-label">
-                Password
-              </label>
-              <input
-                type="password"
-                autoComplete="off"
-                name="password"
-                placeholder="Password"
-                id="password"
-              />
-            </div>
-            <div className="modal-buttons">
-              <button type="submit" className="input-button">
-                <Link to="/profile">Login</Link>
-              </button>
-            </div>
-          </form>
+          <div className="modal-left">
+            <h1 className="modal-title">Welcome!</h1>
+            <form onSubmit={handleSubmit}>
+              <div className="input-block">
+                <label htmlFor="email" className="input-label">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  autoComplete="off"
+                  name="email"
+                  placeholder="Email"
+                  id="email"
+                />
+              </div>
+              <div className="input-block">
+                <label htmlFor="password" className="input-label">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  autoComplete="off"
+                  name="password"
+                  placeholder="Password"
+                  id="password"
+                />
+              </div>
+              <div className="modal-buttons">
+                <button type="submit" className="input-button">
+                  Login
+                </button>
+              </div>
+            </form>
+          </div>
+          <div className="modal-right">
+            <img src={singupimage} alt="" />
+          </div>
         </div>
       </div>
     </div>
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,58 +1,63 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import singupimage from "../assets/image1.jpg";
 const Signup = () => {
   return (
     <div className="container">
       <div className="modal">
         <div className="modal-container">
-          <h1 className="modal-title">Welcome!</h1>
-          <form>
-            <div className="input-block">
-              <label htmlFor="name" className="input-label">
-                Name
-              </label>
-              <input
-                type="name"
-                autoComplete="off"
-                name="name"
-                placeholder="Name"
-                id="name"
-              />
-            </div>
-            <div className="input-block">
-              <label htmlFor="email" className="input-label">
-                Email
-              </label>
-              <input
-                type="email"
-                autoComplete="off"
-                name="email"
-                placeholder="Email"
-                id="email"
-              />
-            </div>
-            <div className="input-block">
-              <label htmlFor="password" className="input-label">
-                Password
-              </label>
-              <input
-                type="password"
-                autoComplete="off"
-                name="password"
-                placeholder="Password"
-                id="password"
-              />
-            </div>
-            <div className="modal-buttons">
-              <button type="submit" className="input-button">
-                Register
-              </button>
-            </div>
-          </form>
-          <p className="sign-up">
-            Already have an account? <Link to="/login"> Login</Link>
-          </p>
+          <div className="modal-left">
+            <h1 className="modal-title">Welcome!</h1>
+            <form>
+              <div className="input-block">
+                <label htmlFor="name" className="input-label">
+                  Name
+                </label>
+                <input
+                  type="name"
+                  autoComplete="off"
+                  name="name"
+                  placeholder="Name"
+                  id="name"
+                />
+              </div>
+              <div className="input-block">
+                <label htmlFor="email" className="input-label">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  autoComplete="off"
+                  name="email"
+                  placeholder="Email"
+                  id="email"
+                />
+              </div>
+              <div className="input-block">
+                <label htmlFor="password" className="input-label">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  autoComplete="off"
+                  name="password"
+                  placeholder="Password"
+                  id="password"
+                />
+              </div>
+              <div className="modal-buttons">
+                <button type="submit" className="input-button">
+                  Register
+                </button>
+              </div>
+            </form>
+            <p className="sign-up">
+              Already have an account? <Link to="/login">Login</Link>
+            </p>
+          </div>
+          <div className="modal-right">
+            <img src={singupimage} alt="" />
+          </div>
         </div>
       </div>
     </div>
