15755bdc6a44c7b3a5c87da62feed8fb6f115d7b
shashankmadaan379 <codebreaker379@gmail.com>
2023-05-07 15:29:34 +0530

    created navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,22 @@
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import HomePage from "./pages/HomePage";
+import Profile from "./pages/Profile";
+
 function App() {
-  return <div>Welcome</div>;
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/register" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../App.css";
+const Navbar = () => {
+  return (
+    <div>
+      <div className="navbar-container">
+        <div className="logo">Eternalight Infotech</div>
+        <div className="menu">
+          <ul className="navbar-items">
+            <li className="navbar-list-items">
+              <Link className="navbar-link" to="/profile">
+                Profile
+              </Link>
+            </li>
+            <li className="navbar-list-items">
+              <Link className="navbar-link" to="/login">
+                Login
+              </Link>
+            </li>
+            <li className="navbar-list-items">
+              <Link className="navbar-link" to="/register">
+                Register
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
