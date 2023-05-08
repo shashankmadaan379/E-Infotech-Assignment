6d3fe1e533dd8b672c3d610781b8d322615234b9
shashankmadaan379 <codebreaker379@gmail.com>
2023-05-08 20:38:27 +0530

    Converted normal form to Formik form, formik with yup

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,18 +1,21 @@
-import React, { useState } from "react";
+import React from "react";
 import signupimage from "../assets/image1.jpg";
 import { Link, useNavigate } from "react-router-dom";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 import axios from "axios";
+import * as Yup from "yup";
+export const validationSchema = Yup.object({
+  email: Yup.string().email().required("Please enter your email"),
+  password: Yup.string().min(6).required("Please enter your password"),
+});
 const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
   const navigate = useNavigate();
-  async function handleSubmit(event) {
-    event.preventDefault();
+  const handleSubmit = async (values) => {
     try {
       const loginUrl = "http://localhost:8000/api/v1/loginUser";
       const response = await axios.post(loginUrl, {
-        email,
-        password,
+        email: values.email,
+        password: values.password,
       });
       if (response) {
         localStorage.setItem("userId", JSON.stringify(response.data.user));
@@ -24,48 +27,66 @@ const Login = () => {
       console.log(error);
       alert("Login failed, Please try again...");
     }
-  }
+  };
   return (
     <div className="container">
       <div className="modal">
         <div className="modal-container">
           <div className="modal-left">
             <h1 className="modal-title">Login</h1>
-            <form onSubmit={handleSubmit}>
-              <div className="input-block">
-                <label htmlFor="email" className="input-label">
-                  Email
-                </label>
-                <input
-                  type="email"
-                  autoComplete="off"
-                  name="email"
-                  placeholder="Email"
-                  id="email"
-                  onChange={(event) => setEmail(event.target.value)}
-                  value={email}
-                />
-              </div>
-              <div className="input-block">
-                <label htmlFor="password" className="input-label">
-                  Password
-                </label>
-                <input
-                  type="password"
-                  autoComplete="off"
-                  name="password"
-                  placeholder="Password"
-                  id="password"
-                  onChange={(event) => setPassword(event.target.value)}
-                  value={password}
-                />
-              </div>
-              <div className="modal-buttons">
-                <button type="submit" className="input-button">
-                  Login
-                </button>
-              </div>
-            </form>
+            <Formik
+              initialValues={{
+                email: "",
+                password: "",
+              }}
+              validationSchema={validationSchema}
+              onSubmit={handleSubmit}>
+              {(formik) => (
+                <Form onSubmit={formik.handleSubmit}>
+                  <div className="input-block">
+                    <label htmlFor="email" className="input-label">
+                      Email
+                    </label>
+                    <Field
+                      type="email"
+                      autoComplete="off"
+                      name="email"
+                      placeholder="Email"
+                      id="email"
+                      className="form-control"
+                    />
+                    <ErrorMessage
+                      name="email"
+                      component="div"
+                      className="error-message"
+                    />
+                  </div>
+                  <div className="input-block">
+                    <label htmlFor="password" className="input-label">
+                      Password
+                    </label>
+                    <Field
+                      type="password"
+                      autoComplete="off"
+                      name="password"
+                      placeholder="Password"
+                      id="password"
+                      className="form-control"
+                    />
+                    <ErrorMessage
+                      name="password"
+                      component="div"
+                      className="error-message"
+                    />
+                  </div>
+                  <div className="modal-buttons">
+                    <button type="submit" className="input-button">
+                      Login
+                    </button>
+                  </div>
+                </Form>
+              )}
+            </Formik>
             <p className="back-to-home">
               Back to Home Page ? <Link to="/">Home</Link>
             </p>
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,20 +1,27 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import singupimage from "../assets/image1.jpg";
 import axios from "axios";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+export const validationSchema = Yup.object({
+  name: Yup.string().min(2).max(25).required("Please enter your name"),
+  email: Yup.string().email().required("Please enter your email"),
+  password: Yup.string().min(6).required("Please enter your password"),
+  confirm_password: Yup.string()
+    .required()
+    .oneOf([Yup.ref("password"), null], "Password must match"),
+});
+
 const Signup = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
   const navigate = useNavigate();
-  async function handleSubmit(event) {
-    event.preventDefault();
+  const handleSubmit = async (values) => {
     try {
       const registerUrl = "http://localhost:8000/api/v1/registerUser";
       const response = await axios.post(registerUrl, {
-        name,
-        email,
-        password,
+        name: values.name,
+        email: values.email,
+        password: values.password,
       });
       if (response) {
         alert("User Registration Successfull !");
@@ -24,62 +31,104 @@ const Signup = () => {
       console.log(error);
       alert("Registration failed, Please try again...");
     }
-  }
+  };
+
   return (
     <div className="container">
       <div className="modal">
         <div className="modal-container">
           <div className="modal-left">
             <h1 className="modal-title">Register</h1>
-            <form onSubmit={handleSubmit}>
-              <div className="input-block">
-                <label htmlFor="name" className="input-label">
-                  Name
-                </label>
-                <input
-                  type="name"
-                  autoComplete="off"
-                  name="name"
-                  placeholder="Name"
-                  id="name"
-                  onChange={(event) => setName(event.target.value)}
-                  value={name}
-                />
-              </div>
-              <div className="input-block">
-                <label htmlFor="email" className="input-label">
-                  Email
-                </label>
-                <input
-                  type="email"
-                  autoComplete="off"
-                  name="email"
-                  placeholder="Email"
-                  id="email"
-                  onChange={(event) => setEmail(event.target.value)}
-                  value={email}
-                />
-              </div>
-              <div className="input-block">
-                <label htmlFor="password" className="input-label">
-                  Password
-                </label>
-                <input
-                  type="password"
-                  autoComplete="off"
-                  name="password"
-                  placeholder="Password"
-                  id="password"
-                  onChange={(event) => setPassword(event.target.value)}
-                  value={password}
-                />
-              </div>
-              <div className="modal-buttons">
-                <button type="submit" className="input-button">
-                  Register
-                </button>
-              </div>
-            </form>
+            <Formik
+              initialValues={{
+                name: "",
+                email: "",
+                password: "",
+                confirm_password: "",
+              }}
+              validationSchema={validationSchema}
+              onSubmit={handleSubmit}>
+              {(formik) => (
+                <Form onSubmit={formik.handleSubmit}>
+                  <div className="input-block">
+                    <label htmlFor="name" className="input-label">
+                      Name
+                    </label>
+                    <Field
+                      type="text"
+                      name="name"
+                      placeholder="Name"
+                      autoComplete="off"
+                      id="name"
+                      className="form-control"
+                    />
+                    <ErrorMessage
+                      name="name"
+                      component="div"
+                      className="error-message"
+                    />
+                  </div>
+                  <div className="input-block">
+                    <label htmlFor="email" className="input-label">
+                      Email
+                    </label>
+                    <Field
+                      type="email"
+                      name="email"
+                      autoComplete="off"
+                      placeholder="Email"
+                      id="email"
+                      className="form-control"
+                    />
+                    <ErrorMessage
+                      name="email"
+                      component="div"
+                      className="error-message"
+                    />
+                  </div>
+                  <div className="input-block">
+                    <label htmlFor="password" className="input-label">
+                      Password
+                    </label>
+                    <Field
+                      type="password"
+                      name="password"
+                      autoComplete="off"
+                      placeholder="Password"
+                      id="password"
+                      className="form-control"
+                    />
+                    <ErrorMessage
+                      name="password"
+                      component="div"
+                      className="error-message"
+                    />
+                  </div>
+                  <div className="input-block">
+                    <label htmlFor="confirm_password" className="input-label">
+                      Confirm Password
+                    </label>
+                    <Field
+                      type="password"
+                      name="confirm_password"
+                      placeholder="Confirm Password"
+                      id="confirm_password"
+                      className="form-control"
+                    />
+                    <ErrorMessage
+                      name="confirm_password"
+                      component="div"
+                      className="error-message"
+                    />
+                  </div>
+                  <div className="modal-buttons">
+                    <button type="submit" className="input-button">
+                      Register
+                    </button>
+                  </div>
+                </Form>
+              )}
+            </Formik>
             <p className="sign-up">
               Already have an account? <Link to="/login">Login</Link>
             </p>
